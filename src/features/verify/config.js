916e235748731c31bd999e31a34253ce8173b2ac
maxunknowncode <221712067+maxunknowncode@users.noreply.github.com>
2025-08-26 05:47:44 +0200

    Update config.js

--- src/features/verify/config.js
+++ src/features/verify/config.js
@@ -1,5 +1,5 @@
 export const VERIFY_CHANNEL_ID = '1354914940611330239';
 export const VERIFY_ROLE_ID = '1354909911691038862';
-export const VERIFY_MESSAGE_ID = '';
+export const VERIFY_MESSAGE_ID = '1409745967883747491';
 export const VERIFY_BUTTON_ID = 'verify_click';
 export const VERIFY_EMOJI = { id: '1355265228862001202', animated: true };
