bb4081c9ed8b885747d7fc0a15fea1749f73f0b5
maxunknowncode <221712067+maxunknowncode@users.noreply.github.com>
2025-08-30 13:16:03 +0200

    Update config.js

--- src/modules/rules/config.js
+++ src/modules/rules/config.js
@@ -2,7 +2,7 @@
 ### Zweck: Hält nur IDs/Konstanten für das Rules-Feature (Channel, Nachricht, Buttons, Default-Sprache, Reset-Zeit).
 */
 export const RULES_CHANNEL_ID = '1354914857538945205';
-export const RULES_MESSAGE_ID = '';
+export const RULES_MESSAGE_ID = '1411308407088087050';
 export const RULES_BUTTON_ID_EN = 'rules_lang_en';
 export const RULES_BUTTON_ID_DE = 'rules_lang_de';
 export const RULES_DEFAULT_LANG = 'en';
